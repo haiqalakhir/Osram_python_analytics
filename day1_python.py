b047ced6a4444c931d3742010c44fdc1295bb67b
haiqalakhir <110401243+haiqalakhir@users.noreply.github.com>
2025-07-28 12:26:36 +0800

    Update day1_python.py

--- day1_python.py
+++ day1_python.py
@@ -4,3 +4,19 @@ print(temp_readings[2])
 for temp in temp_readings:
   if temp > 25.0:
     print(f'High Temp Alert: {temp}°C')
+
+#°
+
+machine_id = ["1AVI1805", "1AVI1811", "1AVI0225", "1AVI0244"]
+for machine in machine_id:
+  print(f'Machine ID: {machine}')
+
+def check_temp(temperature, min_temp=20.0, max_temp=26.0):
+  if temperature < min_temp or temperature > max_temp:
+    print("Not safe")
+  else:
+    print("good to go")
+
+temps1 = temp_readings[0]
+temps2 = temp_readings[2]
+check_temp(temps1)
