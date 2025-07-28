20ebd7958402004eecb51152e7974058148aed08
haiqalakhir <110401243+haiqalakhir@users.noreply.github.com>
2025-07-28 12:29:18 +0800

    Update day1_numpy.py

--- day1_numpy.py
+++ day1_numpy.py
@@ -12,3 +12,22 @@ if os.path.exists(file_name):
   if df is not None:
     print("\nDataFrame loaded successfully.")
     
+#print(df.head()) #show 1st 5 row only
+#print(df.info()) #show data type
+#print(df.describe()) #show statistic
+
+#selected_columns = df[['Idx', 'T1']] #select certain column
+#print(selected_columns.head())
+
+#temp_slice = df.loc[0:100, 'Idx':'T3'] #select certain row
+#print("\nFirst 100 rows and temp columns:")
+#print(temp_slice)
+
+hot_points = df[df['T1'] > 25.0] #select certain column with certain value (filter)
+#print(hot_points)
+
+#T2_T1 = df[df['T2-T1'] < 1.5]
+#print(T2_T1)  # show dulu
+#print(T2_T1.describe())  # then count
+
+hot_points.sort_values(by='T2-T1', ascending=False)
